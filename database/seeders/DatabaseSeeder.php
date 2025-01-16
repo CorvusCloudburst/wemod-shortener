a25b6f120e3d404e1257e4b4859586780b3d8084
CorvusCloudburst <halite.ann@gmail.com>
2025-01-15 22:48:51 -0500

    update seeding

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,7 +5,6 @@ namespace Database\Seeders;
 use App\Models\ShortUrl;
 use App\Models\UrlVisit;
 use App\Models\User;
-// use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Str;
