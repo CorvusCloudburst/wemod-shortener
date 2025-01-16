aa40412dc450e6e7795d194100050481b6fe11d6
CorvusCloudburst <halite.ann@gmail.com>
2025-01-15 23:08:55 -0500

    update nav

--- resources/js/Layouts/NavLayout.jsx
+++ resources/js/Layouts/NavLayout.jsx
@@ -8,9 +8,10 @@ export default function NavLayout({ header, children }) {
         <div className="flex min-h-screen min-w-full flex-col items-center bg-gray-950 justify-start pt-0">
             <nav className="fixed z-50 min-w-full border-b border-gray-500 bg-gray-900">
                 <div className="flex w-full h-16 justify-between mx-auto w-full px-4">
-                        <a href="https://corvus.rip" className="w-full max-w-10 h-full max-h-10 my-auto mr-6" >
-                            <img src="/cloudburstLogo.png" className="w-full h-full opacity-50 hover:opacity-75" />
-                        </a>
+                    {/* Home Icon (Cheeky link to my resume page) */}
+                    <a href="https://corvus.rip" className="w-full max-w-10 h-full max-h-10 my-auto mr-6" >
+                        <img src="/cloudburstLogo.png" className="w-full h-full opacity-50 hover:opacity-75" />
+                    </a>
                     <div className="w-full flex flex-row space-between">
                         {/* Left Side Nav */}
                         <div className="w-full flex flex-row space-x-8">
